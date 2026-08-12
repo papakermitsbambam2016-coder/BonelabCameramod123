7f63d161ff31cd888d1e33590458fa802ccbb4e3
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-12 18:02:33 -0400

    Create PluginInfo.cs

---
+++ src/PluginInfo.cs
@@ -0,0 +1,8 @@
+namespace QuestCamera;
+
+internal static class PluginInfo
+{
+    public const string Name = "QuestCamera";
+    public const string Version = "0.1.0";
+    public const string Author = "QuestCamera";
+}
