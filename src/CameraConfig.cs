4d2f110c245df184478986d43f779cec0a3db0c5
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-12 18:03:55 -0400

    Create CameraConfig.cs

---
+++ src/CameraConfig.cs
@@ -0,0 +1,22 @@
+namespace QuestCamera;
+
+internal static class CameraConfig
+{
+    public static bool AutoSpawn = true;
+
+    public static float SpawnDistance = 2.0f;
+
+    public static float CameraFov = 60f;
+
+    public static float MinFov = 20f;
+
+    public static float MaxFov = 100f;
+
+    public static float ZoomAmount = 5f;
+
+    public static bool StartFrozen = false;
+
+    public static bool EnableRecording = true;
+
+    public static bool EnablePhotos = true;
+}
